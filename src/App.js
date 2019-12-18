e8f99ef470222cd6b59484120af3fa8cce343ca2
Vincent Sanders <vincesanders@hotmail.com>
2019-12-18 15:10:53 -0600

    Added styled-components dependency and added some styles to the form and index.css file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+
+const MainPage = styled.div`
+
+`
 
 function App() {
   const [teamList, setTeamList] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FormContainer = styled.form`
+    width: 500px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 50px;
+`
+
+const Input = styled.input`
+    width: 400px;
+    margin-bottom: 1rem;
+`
 
 export default () => {
     return (
-        <form>
+        <FormContainer>
             <label htmlFor='name' >Full Name</label>
-            <input id='name' type='text' name='name' placeholder='Full Name' />
+            <Input id='name' type='text' name='name' placeholder='Full Name' />
             <label htmlFor='email' >E-Mail</label>
-            <input id='email' type='email' name='email' placeholder='member@email.com' />
+            <Input id='email' type='email' name='email' placeholder='member@email.com' />
             <label htmlFor='role' >Role</label>
-            <input id='role' type='text' name='role' placeholder="Member's role" />
+            <Input id='role' type='text' name='role' placeholder="Member's role" />
             <button>Add Team Member</button>
-        </form>
+        </FormContainer>
     );
 }
